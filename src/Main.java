3dac091c1b85c2e3bfbd68ae34e3401149e94bf0
Mikhail <Mishamengoll@yandex.ru>
2025-09-22 21:16:48 +0300

    test: реализовать пользовательский сценарий в классе Main

--- src/Main.java
+++ src/Main.java
@@ -2,165 +2,56 @@ import manager.Managers;
 import manager.TaskManager;
 import tasks.*;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        Task washDishes = new Task("Помыть посуду", "Нужно нанести мыло для посуды на губку, " +
-            "брать посуду одну за другой, намыливать их губкой, " +
-            "а затем смывать мыло и класть посуду на место", TaskStatuses.NEW);
-        Task assemblePuzzle = new Task("Собрать пазл", "Нужно разложить все пазлины, " +
-            "потом совмещать между собой детали которые подходят друг к другу и делают картинку цельной",
-            TaskStatuses.IN_PROGRESS);
-        Epic buyGroceries = new Epic("Купить продукты", "Купить продкты домой");
-        Subtask writeList = new Subtask("Написать список", "Написать список продуктов," +
-            " которые нужно купить", TaskStatuses.IN_PROGRESS, buyGroceries);
-        Subtask goToGroceryStore = new Subtask("Пойти в магазин",
-            "Пойти в продуктовый магазин и купить там все продукты из списка", TaskStatuses.NEW,
-            buyGroceries);
-        Epic refillCarGasTank = new Epic("Пополнить запасы бензина",
-            "Пополнить запасы бензина в баке машины");
-        Subtask fillUpAtGasStation = new Subtask("Заправиться на заправке",
-            "Подъехать к колонке, выбрать нужный бензин и заправить бак необходимым количеством бензина",
-            TaskStatuses.NEW, refillCarGasTank);
+        Epic epic1 = new Epic("1", "1");
+        Subtask subtask1 = new Subtask("2", "2", TaskStatuses.NEW, epic1);
+        Subtask subtask2 = new Subtask("3", "3", TaskStatuses.IN_PROGRESS, epic1);
+        Subtask subtask3 = new Subtask("4", "4", TaskStatuses.DONE, epic1);
+        Epic epic2 = new Epic("5", "5");
 
         TaskManager taskManager = Managers.getDefault();
-        taskManager.putTask(washDishes);
-        taskManager.putTask(assemblePuzzle);
-        taskManager.putEpic(buyGroceries);
-        taskManager.putSubtask(writeList);
-        taskManager.putSubtask(goToGroceryStore);
-        taskManager.putEpic(refillCarGasTank);
-        taskManager.putSubtask(fillUpAtGasStation);
+        taskManager.putEpic(epic1);
+        taskManager.putSubtask(subtask1);
+        taskManager.putSubtask(subtask2);
+        taskManager.putSubtask(subtask3);
+        taskManager.putEpic(epic2);
 
-        System.out.println("Списки эпиков, задач и подзадач:");
-        System.out.println(taskManager.getEpics());
-        System.out.println(taskManager.getTasks());
-        System.out.println(taskManager.getSubtasks());
-        System.out.println();
-
-
-        taskManager.getTaskById(1);
+        taskManager.getEpicById(5);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getSubtaskById(4);
+        taskManager.getSubtaskById(3);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getEpicById(3);
+        taskManager.getEpicById(5);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getTaskById(1);
+        taskManager.getSubtaskById(2);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-
-        Task newWashDishes = new Task(washDishes, washDishes.getName(), washDishes.getDescription(),
-            TaskStatuses.IN_PROGRESS);
-        Task newAssemblePuzzle = new Task(assemblePuzzle, assemblePuzzle.getName(), assemblePuzzle.getDescription(),
-            TaskStatuses.DONE);
-        Subtask newWriteList = new Subtask(writeList, writeList.getName(), writeList.getDescription(),
-            TaskStatuses.DONE);
-        Subtask newGoToGroceryStore = new Subtask(goToGroceryStore, goToGroceryStore.getName(),
-            goToGroceryStore.getDescription(), TaskStatuses.IN_PROGRESS);
-        Subtask newFillUpAtGasStation = new Subtask(fillUpAtGasStation, fillUpAtGasStation.getName(),
-            fillUpAtGasStation.getDescription(), TaskStatuses.DONE);
-
-        taskManager.renewTask(newWashDishes);
-        taskManager.renewTask(newAssemblePuzzle);
-        taskManager.renewSubtask(newWriteList);
-        taskManager.renewSubtask(newGoToGroceryStore);
-        taskManager.renewSubtask(newFillUpAtGasStation);
-
-        System.out.println("Списки эпиков, задач и подзадач после изменения статусов у задач и подзадач:");
-        System.out.println(taskManager.getEpics());
-        System.out.println(taskManager.getTasks());
-        System.out.println(taskManager.getSubtasks());
-        System.out.println();
-
-
-        taskManager.getTaskById(1);
+        taskManager.getEpicById(1);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getSubtaskById(5);
+        taskManager.getSubtaskById(2);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getEpicById(6);
+        taskManager.getSubtaskById(3);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-
-        taskManager.removeTaskById(1);
-        taskManager.removeEpicById(3);
-
-        System.out.println("Списки эпиков, задач и подзадач после удаления одной из задач и одного из эпиков:");
-        System.out.println(taskManager.getEpics());
-        System.out.println(taskManager.getTasks());
-        System.out.println(taskManager.getSubtasks());
-        System.out.println();
-
-
-        taskManager.getTaskById(2);
+        taskManager.removeEpicById(5);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
 
-        taskManager.getSubtaskById(7);
+        taskManager.removeEpicById(1);
         System.out.println("История просмотров:");
         System.out.println(taskManager.getHistory());
-        System.out.println();
-
-        taskManager.getEpicById(6);
-        System.out.println("История просмотров:");
-        System.out.println(taskManager.getHistory());
-        System.out.println();
-
-        taskManager.getTaskById(2);
-        System.out.println("История просмотров:");
-        System.out.println(taskManager.getHistory());
-        System.out.println();
-
-        taskManager.getEpicById(6);
-        System.out.println("История просмотров:");
-        System.out.println(taskManager.getHistory());
-        System.out.println();
-
-        printAllTasks(taskManager);
-    }
-
-    private static void printAllTasks(TaskManager manager) {
-        System.out.println("Задачи:");
-        for (Task task : manager.getTasks()) {
-            System.out.println(task);
-        }
-        System.out.println("Эпики:");
-        for (Task epic : manager.getEpics()) {
-            System.out.println(epic);
-
-            for (Task task : manager.getEpicSubtasks((Epic) epic)) {
-                System.out.println("--> " + task);
-            }
-        }
-        System.out.println("Подзадачи:");
-        for (Task subtask : manager.getSubtasks()) {
-            System.out.println(subtask);
-        }
-
-        System.out.println("История:");
-        for (Task task : manager.getHistory()) {
-            System.out.println(task);
-        }
     }
 }
